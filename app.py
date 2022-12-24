89f0f70b2c74973261f7a0e3f11507e59c0407c1
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2022-12-24 05:22:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 # Autenticación de OpenAI
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
- st.title("Evaluador de textos con GPT-3")
+st.title("Evaluador de textos con GPT-3")
 
 def evaluar_texto(texto):
     # Utiliza GPT-3 para evaluar el texto
@@ -24,8 +24,6 @@ def evaluar_texto(texto):
 
 def main():
    
- st.title("Evaluador de textos con GPT-3")
-
     texto = st.text_area("Ingresa el texto a evaluar. Al finalizar, Ctrl+Enter")
     if texto:
         respuesta = evaluar_texto(texto)
