c365f128504afa0a1a07addae8dd22c67d6c353d
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2022-12-25 18:20:30 -0600

    Update app3.py

--- app3.py
+++ app3.py
@@ -25,9 +25,9 @@ def main():
     st.caption("Evalúa la corrección gramatical y la riqueza léxica de textos en español")
     
     # Muestra un menú con dos opciones: subir archivo o pegar texto
-    st.subheader("Evaluador")
     opcion = st.sidebar.radio("Selecciona una opción:", ("Subir archivo", "Pegar texto"))
-
+    st.subheader("Evaluador")
+    
     if opcion == "Subir archivo":
         uploaded_file = st.file_uploader("Selecciona un archivo de texto (extensión .txt):")
 
