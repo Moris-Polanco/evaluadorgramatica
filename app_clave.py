b6f6146e28aa0c8f560230191206f4881d09fc7a
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2022-12-25 19:24:57 -0600

    Update app_clave.py

--- app_clave.py
+++ app_clave.py
@@ -16,12 +16,6 @@ def evaluar_texto(texto):
     # Devuelve la respuesta de GPT-3
     return respuesta
 
-def ocultar_clave(clave):
-    # Reemplaza cada carácter de la clave por un punto
-    clave_oculta = "." * len(clave)
-
-    return clave_oculta
-
 def main():
     st.title("Evaluador de textos con GPT-3")
 
@@ -29,42 +23,34 @@ def main():
     st.sidebar.subheader("Evaluador")
 
     # Pide la clave de OpenAI en la barra lateral
-    openai_key_input = st.sidebar.text_input("Ingresa tu clave de OpenAI:")
+    openai_key_input = st.sidebar.text_input("Ingresa tu clave de OpenAI:", type='password')
     openai_key = openai_key_input.value
 
-    # Agrega un evento de cambio de texto al widget de entrada de texto
-    # para reemplazar los caracteres de la clave por puntos
-    @openai_key_input.on_change
-    def reemplazar_por_puntos(change):
-        openai_key_input.text = ocultar_clave(openai_key)
-
     if openai_key:
-        openai.api_key = openai_key 
-        
+        openai.api_key = openai_key
+
     # Muestra un menú con dos opciones: subir archivo o pegar texto
-    opcion = st.sidebar.radio("Selecciona una opción:", ("Subir archivo", "Pegar texto"))
+    opcion = st.sidebar.radio("Selecciona una opción:", ["Subir archivo", "Pegar texto"])
 
     if opcion == "Subir archivo":
-        uploaded_file = st.file_uploader("Selecciona un archivo:")
+        # Pide al usuario que seleccione un archivo
+        archivo = st.file_uploader("Selecciona un archivo:")
 
-        if uploaded_file is not None:
-            # Lee el contenido del archivo como una cadena de bytes
-            file_content = uploaded_file.read()
+        if archivo is not None:
+            # Lee el contenido del archivo y lo almacena en una variable
+            with open(archivo, "r") as f:
+                texto = f.read()
 
-            # Convierte la cadena de bytes a una cadena de texto
-            # Utiliza la codificación utf-8 por defecto, pero puedes especificar otra
-            # si el archivo tiene una codificación diferente
-            file_content_text = file_content.decode("utf-8")
-
-            # Evalúa el contenido del archivo
-            respuesta = evaluar_texto(file_content_text)
+            # Evalúa el texto y muestra la respuesta de GPT-3
+            respuesta = evaluar_texto(texto)
             st.markdown(respuesta)
-
-    elif opcion == "Pegar texto":
+    else:
+        # Pide al usuario que ingrese el texto
         texto = st.text_area("Ingresa el texto a evaluar. Al finalizar, Ctrl+Enter")
+
         if texto:
+            # Evalúa el texto y muestra la respuesta de GPT-3
             respuesta = evaluar_texto(texto)
             st.markdown(respuesta)
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__
