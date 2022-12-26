64729471fc29d3fd4a0d4a7c0590489aad783a2a
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2022-12-25 18:27:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,34 @@ def evaluar_texto(texto):
 
 def main():
     st.title("Evaluador de textos con GPT-3")
+    st.caption("Evalúa la corrección gramatical y la riqueza léxica de textos en español")
+    
+    # Muestra un menú con dos opciones: subir archivo o pegar texto
+    st.sidebar.subheader("Evaluador")
+    opcion = st.sidebar.radio("Selecciona una opción:", ("Subir archivo", "Pegar texto"))
+    st.sidebar.caption("por Moris Polanco")
+    
+    if opcion == "Subir archivo":
+        uploaded_file = st.file_uploader("Selecciona un archivo de texto (extensión .txt):")
 
-    texto = st.text_area("Ingresa el texto a evaluar. Al finalizar, Ctrl+Enter")
-    if texto:
-        respuesta = evaluar_texto(texto)
-        st.markdown(respuesta)
+        if uploaded_file is not None:
+            # Lee el contenido del archivo como una cadena de bytes
+            file_content = uploaded_file.read()
+
+            # Convierte la cadena de bytes a una cadena de texto
+            # Utiliza la codificación utf-8 por defecto, pero puedes especificar otra
+            # si el archivo tiene una codificación diferente
+            file_content_text = file_content.decode("utf-8")
+
+            # Evalúa el contenido del archivo
+            respuesta = evaluar_texto(file_content_text)
+            st.markdown(respuesta)
+
+    elif opcion == "Pegar texto":
+        texto = st.text_area("Ingresa el texto a evaluar. Al finalizar, Ctrl+Enter")
+        if texto:
+            respuesta = evaluar_texto(texto)
+            st.markdown(respuesta)
 
 
 if __name__ == "__main__":
