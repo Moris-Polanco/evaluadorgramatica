3c10641f989750b08ca74d9369e72f92d54b3b29
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2022-12-25 19:04:16 -0600

    Update app_clave.py

--- app_clave.py
+++ app_clave.py
@@ -24,7 +24,8 @@ def main():
     st.sidebar.subheader("Evaluador")
 
     # Pide la clave de OpenAI en la barra lateral
-    openai_key = st.sidebar.text_input("Ingresa tu clave de OpenAI:", textPassword)
+    openai_key = st.sidebar.text_input("Ingresa tu clave de OpenAI:", password=True)
+
     if openai_key:
         openai.api_key = openai_key
 
